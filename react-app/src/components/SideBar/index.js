3a161de4db02e5eda4f4e24411c59dbf50f58771
Seth <switfoth@gmail.com>
2021-02-03 16:15:29 -0600

    Missed two untracked files with last update

---
+++ react-app/src/components/SideBar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./sidebar.css";
+
+const SideBar = ()=>{
+
+
+    return (
+            <div id='sidebar'>
+                <h1>Side Bar</h1>
+            </div>
+    )
+}
+
+export default SideBar
