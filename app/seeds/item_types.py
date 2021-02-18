12b1d4a1d76d5a56d06d46ec09584acce373c81b
Seth <switfoth@gmail.com>
2021-02-17 19:00:51 -0600

    Adding seed files

---
+++ app/seeds/item_types.py
@@ -0,0 +1,69 @@
+from app.models import db, ItemType
+
+def seed_item_types():
+    weapon = ItemType(
+        name="Weapon"
+    )
+
+    armor = ItemType(
+        name="Armor"
+    )
+
+    ring = ItemType(
+        name="Ring"
+    )
+
+    wand = ItemType(
+        name="Wand"
+    )
+
+    wondrous_item = ItemType(
+        name="Wondrous Item"
+    )
+
+    potion = ItemType(
+        name="Potion"
+    )
+
+    gear = ItemType(
+        name="Gear"
+    )
+
+    tool = ItemType(
+        name="Tool"
+    )
+
+    scroll = ItemType(
+        name="Scroll"
+    )
+
+    consumable = ItemType(
+        name="Consumable"
+    )
+
+    vehicle = ItemType(
+        name="Vehicle"
+    )
+
+    other = ItemType(
+        name="...Other?"
+    )
+
+    db.session.add(weapon)
+    db.session.add(armor)
+    db.session.add(ring)
+    db.session.add(wand)
+    db.session.add(wondrous_item)
+    db.session.add(potion)
+    db.session.add(gear)
+    db.session.add(tool)
+    db.session.add(scroll)
+    db.session.add(consumable)
+    db.session.add(vehicle)
+    db.session.add(other)
+
+    db.session.commit()
+
+    def undo_item_types():
+        db.session.execute('TRUNCATE item_types;')
+        db.session.commit()
