2a5d4783e0e3e73e2c23194096458daf8a340852
Seth <switfoth@gmail.com>
2021-03-02 12:44:07 -0600

    New starting point

--- react-app/src/components/Party_Delete_Modal/PartyDeleteForm.js
+++ react-app/src/components/Party_Delete_Modal/PartyDeleteForm.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteSingleItem } from "../../store/item";
 import { deleteSingleParty, deselectParty, getUserParties } from '../../store/party';
-import { deleteSingleMember} from '../../store/member'
 import "./partydeleteform.css";
 
 function PartyDeleteForm() {
@@ -10,20 +8,6 @@ function PartyDeleteForm() {
 
   const user = useSelector(state => state.session.user)
   const currentParty = useSelector(state => state.party.currentParty)
-  const partyMembers = useSelector(state => state.member.memberList)
-  const membersToDelete = partyMembers.map(member =>{
-      if(member.party_id === currentParty){
-          return member
-      }
-    return "Members Deleted"
-  })
-  const currentItems = useSelector(state => state.item.itemList)
-  const itemsToDelete = currentItems.map(item =>{
-      if(item.party_id === currentParty){
-          return item
-      }
-    return "Items Deleted"
-  })
 
   const handleSubmit = (e) => {
     e.preventDefault();
