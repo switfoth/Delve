14f3df46b85d8bbc5b5ee5c599c148f73114efe3
Seth <switfoth@gmail.com>
2021-03-16 19:45:55 -0500

    Added header info

--- react-app/src/components/BasePage/index.js
+++ react-app/src/components/BasePage/index.js
@@ -6,6 +6,14 @@ const BasePage = ()=>{
 
 return (
     <>
+        <head>
+            <meta charset="utf-8" />
+            <title>Delve</title>
+            <meta property="og:image" content="https://i.imgur.com/SfMlija.png" />
+            <meta property="og:description" content="Delve is a loot tracker for Dungeons and Dragons and other D20 based systems that lets you keep track of parties, party members, and the loot associated with them!" />
+            <meta property="og:url"content="http://delve-tracker.herokuapp.com" />
+            <meta property="og:title" content="Delve - Dungeons and Dragons Loot Tracker" />
+        </head>
             <AppWrapper id="root" style={{height: '100%'}}/>
     </>
 )
